7d41c29945cb7e883df142ef60ef86bb14fa5fbd
aning <aning@anningdeMacBook-Air.local>
2026-09-01 19:03:09 +0800

    deploy: e6623ec

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'zhongfu-v34';
+const CACHE_VERSION = 'zhongfu-v36';
 const SHELL_CACHE = `${CACHE_VERSION}-shell`;
 const PAGE_CACHE = `${CACHE_VERSION}-pages`;
 const ASSET_CACHE = `${CACHE_VERSION}-assets`;
